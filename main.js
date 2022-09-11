e4181e0b10b31c400aa4458de2cc3bb993ce9d61
AnayMahajan <86356050+AnayMahajan@users.noreply.github.com>
2022-09-11 16:32:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function preload(){
+clownNose= loadImage('https://i.postimg.cc/jd1rj5tw/Mustache-img-removebg-preview.png')
+}
+
+noseX= 0;
+noseY= 0;
+
+function setup(){
+    canvas= createCanvas(290, 290);
+    canvas.center()
+    video= createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+fill(0, 0, 0);
+image(clownNose, noseX, noseY, 50, 30);
+}
+
+function take_snapshot(){
+    save('Filter_img');
+}
+function modelLoaded(){
+    console.log("Model is Initialized");
+}
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        noseX= results[0].pose.nose.x-20;
+        noseY= results[0].pose.nose.y;
+        console.log("nose x= "+ noseX);
+        console.log("nose y= "+ noseY);
+    }
+}
+
+
